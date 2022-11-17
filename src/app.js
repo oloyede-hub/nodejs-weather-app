b83f5abd47c6b002cbfd12b32f5e584010261d0a
Oloyede Sodiq <sodiqoloyede295@gmail.com>
2022-11-17 21:03:18 +0100

    Editted the Output message in the forecast util

--- src/app.js
+++ src/app.js
@@ -52,19 +52,6 @@ app.get('/about', (req, res) => {
 });
 
 
-// app.get('/products', (req, res) => {
-//     console.log(req.query)
-//     if(!req.query.search) {
-//        return res.send({
-//             error: "You must provide the search term!"
-//         })
-//     }
-//     res.send({
-//         products: []
-
-//     });
-// });
-
 
 app.get("/weather", (req, res) => {
     if(!req.query.address) {
@@ -77,13 +64,14 @@ app.get("/weather", (req, res) => {
             error: error
         });
         }
-      
+
         forecast(latitude, longitude, (error, forecastData) => {
           if(error) {
             return res.send({
                 error: error
             })
           }
+          
           res.send({
             "forecast": forecastData,
             "location": location,
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
         }else if(body.message) {
           callback("Unable to find location", undefined)
         }else {
-          callback(undefined, `${body.weather[0].description} at ${body.main.temp_max} degrees celcius out. There is ${body.main.humidity}% of rain fall`);
+          callback(undefined, `${body.weather[0].description}. The temperature feels like ${body.main.feels_like} but it is ${body.main.temp_max} degrees celcius. There is ${body.main.humidity}% of rain fall. The wind is ${body.wind.deg} degrees and moving at ${body.wind.speed}m/s`);
         }
       })
     }
